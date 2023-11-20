4a2f190cfdb9c9c75c89bffd8e4ad9b1d1265b49
kai <supon3060@gmail.com>
2023-11-20 16:41:08 +0800

    Support positional parameter

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
+# Run tests
+for language in "$@"
+do
+  if [ "${language}" == "jl" ] || [ "${language}" == "julia" ]; then
+    echo "Run julia tests"
+    julia --project=julia --startup-file=no  -e 'using Pkg; Pkg.test()'
+  elif [ "${language}" == "py" ] || [ "${language}" == "python" ]; then
+    echo "Run python tests"
+    echo "Not implemented"
+  elif [ "${language}" == "rs" ] || [ "${language}" == "rust" ]; then
+    echo "Run rust tests"
+    echo "Not implemented"
+  elif [ "${language}" == "all" ]; then
+    echo "Run tests on all implemented code"
+    echo "Not implemented"
+  else
+    echo "Arguments support \`jl|julia\`, \`py|python\`, \`rs|rust\` and \`all\`."
+  fi
+done
 
-julia --project=julia --startup-file=no  -e 'using Pkg; Pkg.test()'
+
