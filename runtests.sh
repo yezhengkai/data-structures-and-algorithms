8760dc9df55a94f9e58e23dbe3afaa7bb23d7a4e
kai <supon3060@gmail.com>
2023-11-30 19:04:21 +0800

    Update `runtests.sh`

--- runtests.sh
+++ runtests.sh
@@ -1,22 +1,42 @@
 #!/usr/bin/env bash
 
+function run_julia_tests(){
+  julia --project=julia --startup-file=no  -e 'using Pkg; Pkg.test()'
+}
+
+function run_python_tests(){
+  pushd python || exit
+  poetry run pytest -v
+  popd || exit
+}
+
+function run_rust_tests(){
+  pushd rust || exit
+  cargo nextest run -v
+  popd || exit
+}
+
+function run_all_tests() {
+  run_julia_tests
+  run_python_tests
+  run_rust_tests
+}
+
 # Run tests
 for language in "$@"
 do
   if [ "${language}" == "jl" ] || [ "${language}" == "julia" ]; then
     echo "Run julia tests"
-    julia --project=julia --startup-file=no  -e 'using Pkg; Pkg.test()'
+    run_julia_tests
   elif [ "${language}" == "py" ] || [ "${language}" == "python" ]; then
     echo "Run python tests"
-    pushd python || exit
-    poetry run pytest
-    popd || exit
+    run_python_tests
   elif [ "${language}" == "rs" ] || [ "${language}" == "rust" ]; then
     echo "Run rust tests"
-    echo "Not implemented"
+    run_rust_tests
   elif [ "${language}" == "all" ]; then
     echo "Run tests on all implemented code"
-    echo "Not implemented"
+    run_all_tests
   else
     echo "Arguments support \`jl|julia\`, \`py|python\`, \`rs|rust\` and \`all\`."
   fi
